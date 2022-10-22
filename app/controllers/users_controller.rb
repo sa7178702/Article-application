af8828d57d060a8875b3512412adc1a34a43be74
sa7178702@gmail.com <sa7178702@gmail.com>
2022-10-23 03:49:20 +0530

    Corrected the code.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :find_user , only: [:show , :edit, :update, :destroy ]
 
-  	def index
+  def index
 		@user = User.all
     @user = @user.paginate(page: params[:page], per_page: 3)
 	end
@@ -12,9 +12,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      # UsermailerMailer.welcome_email(@user).deliver_later
-      UsermailerMailer.welcome_email(@user).deliver
+    if @user.save!
       session[:user_id] = @user.id
       redirect_to users_path
       flash[:notice] = "#{@user.name} Sign Up successful "
@@ -46,12 +44,12 @@ class UsersController < ApplicationController
     end
   end
 
-   def destroy
+  def destroy
        # @user = User.find(params[:id])
-      if @user.destroy
-        redirect_to root_path
-        flash[:notice] = "User Destroyed ? "
-      end
+    if @user.destroy
+      redirect_to root_path
+      flash[:notice] = "User Destroyed ? "
+    end
   end
 
   private
